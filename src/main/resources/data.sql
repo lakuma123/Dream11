8700a4b6d738e1b700d8f0d893ab2444514b8368
Peter <peter.teja9966@gmail.com>
2019-07-13 15:23:10 +0530

    ADDED DEVICE DATA

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,10 @@ values(5, 1,'123','ADMIN','admin1');
 insert into device_type values(1, 'LAPTOP', 'Laptop');
 insert into device_type values(2, 'MOBILE', 'Mobile');
 insert into device_type values(3, 'DESKTOP', 'Desktop');
+
+INSERT INTO DEVICE  VALUES(1,1, {ts '2012-09-17 18:47:52.69'}, 'PRINTER',{ts '2012-09-17 18:47:52.69'},1,1,1,1);
+INSERT INTO DEVICE  VALUES(2,1, {ts '2012-09-17 18:47:52.69'}, 'PRINTER',{ts '2012-09-17 18:47:52.69'},1,1,1,1);
+INSERT INTO DEVICE  VALUES(3,1, {ts '2012-09-17 18:47:52.69'}, 'PRINTER',{ts '2012-09-17 18:47:52.69'},1,1,1,1);
+INSERT INTO DEVICE  VALUES(4,1, {ts '2012-09-17 18:47:52.69'}, 'PRINTER',{ts '2012-09-17 18:47:52.69'},1,1,1,1);
+INSERT INTO DEVICE  VALUES(5,1, {ts '2012-09-17 18:47:52.69'}, 'PRINTER',{ts '2012-09-17 18:47:52.69'},1,1,1,1);
+INSERT INTO DEVICE  VALUES(6,1, {ts '2012-09-17 18:47:52.69'}, 'PRINTER',{ts '2012-09-17 18:47:52.69'},1,1,1,1);
